5e61343c43ebb94b3d915f4a52e2485ec37b26ef
Krzysztof Kalkowski <kkalkowski@pulsonline.pl>
2024-12-20 20:37:12 +0100

    Add function add to cart

--- src/js/script.js
+++ src/js/script.js
@@ -159,6 +159,7 @@
       thisProduct.cartButton.addEventListener('click', function(event){
         event.preventDefault();
         thisProduct.processOrder();
+        thisProduct.addToCart();
       });
       
     }
@@ -169,6 +170,60 @@
       thisProduct.amountWidgetElem.addEventListener('updated', function () {
         thisProduct.processOrder();
       });
+    }
+
+    addToCart(){
+      const thisProduct = this;
+
+      //app.cart.add(thisProduct);
+      app.cart.add(thisProduct.prepareCartProduct());
+    }
+
+    prepareCartProduct(){
+      const thisProduct = this;
+
+      const productSummary = {
+        id: thisProduct.id,
+        name: thisProduct.data.name,
+        amount: thisProduct.amountWidget.input.value,
+        priceSingle: thisProduct.priceSingle,
+        price: thisProduct.price,
+        params: thisProduct.prepareCartProductParams(),
+      };
+
+      return productSummary;
+
+    }
+
+    prepareCartProductParams(){
+      const thisProduct = this;
+
+  const formData = utils.serializeFormToObject(thisProduct.form);
+  const params = {};
+
+  // for very category (param)
+  for(let paramId in thisProduct.data.params) {
+    const param = thisProduct.data.params[paramId];
+
+    // create category param in params const eg. params = { ingredients: { name: 'Ingredients', options: {}}}
+    params[paramId] = {
+      label: param.label,
+      options: {}
+    }
+
+    // for every option in this category
+    for(let optionId in param.options) {
+      const option = param.options[optionId];
+      const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
+      if(optionSelected) {
+        // option is selected!
+        params[paramId].options[optionId] = option.label;
+      }
+    }
+  }
+
+  return params;
     }
 
   processOrder(){
@@ -218,9 +273,11 @@
 
     }
     }
-  
+    thisProduct.priceSingle = price;
+
   // update calculated price in the HTML
   price *= thisProduct.amountWidget.value;
+  thisProduct.price = price;
   thisProduct.priceElem.innerHTML = price;
     }
   }
@@ -289,6 +346,7 @@
       thisCart.dom.wrapper = element;
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger); 
       thisCart.initActions();
+      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
     }
 
     initActions() {
@@ -297,6 +355,14 @@
         event.preventDefault();
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       })
+    }
+
+    add(menuProduct){
+      const thisCart = this;
+      const generatedHTML = templates.cartProduct(menuProduct);
+      const generatedDOMHtml = utils.createDOMFromHTML(generatedHTML);
+      thisCart.dom.productList.appendChild(generatedDOMHtml);
+      //console.log('adding product', menuProduct)
     }
 
   }
